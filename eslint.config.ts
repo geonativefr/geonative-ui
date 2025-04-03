6e592722e7a76ed76cdcadae464afaca55c8c20a
Arthur Remond <arthur.remond@geonative.fr>
2025-04-03 15:04:23 +0200

    chore: add eslint & prettier (#5)

---
+++ eslint.config.ts
@@ -0,0 +1,19 @@
+import { globalIgnores } from 'eslint/config';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVue from 'eslint-plugin-vue';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,vue}'],
+  },
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['/dist/**', '/src/shadcn/**'],
+  },
+  globalIgnores(['dist/**', '/src/shadcn/**']),
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+  skipFormatting
+);
