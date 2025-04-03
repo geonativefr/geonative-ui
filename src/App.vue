13864643aa07fb8ab92dbd6f762756f27175ed9f
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-04-03 16:05:40 +0200

    working progress

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script setup lang="ts"></script>
-
 <template>
   <h1>Geonative UI</h1>
+  <Breadcrumb :items="breadcrumbItems"/>
 </template>
+
+<script setup lang="ts">
+import Breadcrumb from '@/components/breadcrumb/Breadcrumb'
+const breadcrumbItems = [
+  { title: 'Home', link: '/' },
+  { title: 'About', link: '/about' },
+  { title: 'Contact' }
+];
+</script>
 
 <style scoped></style>
---
+++ src/components/breadcrumb/Breadcrumb.vue
@@ -0,0 +1,43 @@
+ <template>
+    <ShadcnBreadcrumb>
+      <ShadcnBreadcrumbList>
+        <ShadcnBreadcrumbItem
+          v-for="(item, index) in items"
+          :key="index"
+          :class="{'hidden md:block': index !== items.length - 1}"
+        >
+          <ShadcnBreadcrumbLink v-if="item.link" :href="item.link">
+            {{ item.title }}
+          </ShadcnBreadcrumbLink>
+          <ShadcnBreadcrumbPage v-else>
+            {{ item.title }}
+          </ShadcnBreadcrumbPage>
+          <ShadcnBreadcrumbSeparator v-if="index !== items.length - 1" class="hidden md:block" />
+        </ShadcnBreadcrumbItem>
+      </ShadcnBreadcrumbList>
+    </ShadcnBreadcrumb>
+  </template>
+
+  <script setup lang="ts">
+  import {
+    Breadcrumb as ShadcnBreadcrumb,
+    BreadcrumbItem as ShadcnBreadcrumbItem,
+    BreadcrumbLink as ShadcnBreadcrumbLink,
+    BreadcrumbList as ShadcnBreadcrumbList,
+    BreadcrumbPage as ShadcnBreadcrumbPage,
+    BreadcrumbSeparator as ShadcnBreadcrumbSeparator,
+  } from '@/shadcn/ui/breadcrumb';
+
+  interface BreadcrumbItem {
+    title: string;
+    link?: string;
+  }
+
+  const props = defineProps<{
+    items: BreadcrumbItem[];
+  }>();
+  </script>
+
+  <style scoped>
+
+  </style>
