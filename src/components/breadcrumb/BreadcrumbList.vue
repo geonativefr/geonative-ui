ca8fe3268a3464ba63df87d85ac9609959667193
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-04-04 16:15:08 +0200

    add isLastIndex in Breadcrumb

--- src/components/breadcrumb/BreadcrumbList.vue
+++ src/components/breadcrumb/BreadcrumbList.vue
@@ -1,24 +1,19 @@
 <template>
   <ShadcnBreadcrumb>
     <ShadcnBreadcrumbList>
-      <Breadcrumb :items="items"/>
+      <Breadcrumb :items="props.items" />
     </ShadcnBreadcrumbList>
   </ShadcnBreadcrumb>
 </template>
 
 <script setup lang="ts">
-import {
-  Breadcrumb as ShadcnBreadcrumb,
-  BreadcrumbList as ShadcnBreadcrumbList,
-} from '@/shadcn/ui/breadcrumb';
-import Breadcrumb from '@/components/breadcrumb/Breadcrumb.vue'
+import { Breadcrumb as ShadcnBreadcrumb, BreadcrumbList as ShadcnBreadcrumbList } from '@/shadcn/ui/breadcrumb';
+import Breadcrumb from '@/components/breadcrumb/Breadcrumb.vue';
 import type { BreadcrumbItem } from '@/type.ts';
 
 const props = defineProps<{
   items: BreadcrumbItem[];
 }>();
-
-const { items } = props;
 </script>
 
 <style scoped></style>
