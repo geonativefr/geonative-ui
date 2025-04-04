3da30944e21d8f6e54e22f58ac317dc017f97cfb
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-04-04 17:49:37 +0200

    add square option for the avatar

--- src/components/avatar/Avatar.vue
+++ src/components/avatar/Avatar.vue
@@ -1,18 +1,25 @@
 <template>
-  <ShadcnAvatar>
-    <ShadcnAvatarImage src="https://github.com/unovue.png" alt="@unovue" />
+  <ShadcnAvatar :class="isSquare ? 'h-8 w-8 rounded-lg' : ''">
+    <ShadcnAvatarImage src="https://avatars.githubusercontent.com/u/167002380?v=4" alt="@unovue" />
     <ShadcnAvatarFallback>A</ShadcnAvatarFallback>
   </ShadcnAvatar>
 </template>
 
 <script setup lang="ts">
+import { defineProps } from 'vue';
 import {
   Avatar as ShadcnAvatar,
   AvatarImage as ShadcnAvatarImage,
   AvatarFallback as ShadcnAvatarFallback,
 } from '@/shadcn/ui/avatar';
+
+const props = defineProps({
+  isSquare: {
+    type: Boolean,
+    default: false,
+  },
+});
 </script>
 
 <style scoped>
-
 </style>
