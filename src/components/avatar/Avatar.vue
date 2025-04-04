8a9abe05611553b26e9aeb975355cb5f24158a56
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-04-04 17:16:08 +0200

    add Avatar components and add Avatar in App.vue

---
+++ src/components/avatar/Avatar.vue
@@ -0,0 +1,18 @@
+<template>
+  <ShadcnAvatar>
+    <ShadcnAvatarImage src="https://github.com/unovue.png" alt="@unovue" />
+    <ShadcnAvatarFallback>A</ShadcnAvatarFallback>
+  </ShadcnAvatar>
+</template>
+
+<script setup lang="ts">
+import {
+  Avatar as ShadcnAvatar,
+  AvatarImage as ShadcnAvatarImage,
+  AvatarFallback as ShadcnAvatarFallback,
+} from '@/shadcn/ui/avatar';
+</script>
+
+<style scoped>
+
+</style>
