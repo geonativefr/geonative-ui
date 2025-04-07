5ffa6c367eaa72efe42e3fe37b48899db6a596ba
LounisBou <lounis.bou@gmail.com>
2025-04-07 12:41:23 +0200

    Create a main Navigation component

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import './style.css';
-import App from './App.vue';
-import routes from './routes';
+import App from '@/App.vue';
+import routes from '@/routes';
 
 const router = createRouter({
   history: createWebHistory(),
