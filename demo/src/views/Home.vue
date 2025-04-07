8b19becb4dbaca4f3a1e943535f4eb289cd90e2e
LounisBou <lounis.bou@gmail.com>
2025-04-07 15:01:37 +0200

    Add Markdown of README content to Home page of demo project

--- demo/src/views/Home.vue
+++ demo/src/views/Home.vue
@@ -1,1 +1,16 @@
-<template></template>
+<template>
+  <div class="flex flex-col items-center w-full">
+    <div class="flex flex-col items-center justify-center w-full max-w-4xl px-4 mx-auto">
+      <Markdown :content="content" class="markdown-body" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Markdown from '@/components/markdown/Markdown.vue';
+import { ref } from 'vue';
+import readmeContent from '../../../README.md?raw'; // Import the README file as raw text using Vite's raw loader
+
+const content = ref(readmeContent);
+
+</script>
