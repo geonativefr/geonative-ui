4e3e4c3712c3760c5e65746e7c3859d3ddfbea31
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-04-09 16:57:32 +0200

    add DropdownMenu components + add DropdownMenu to view

---
+++ src/components/dropdown-menu/DropdownMenuCategory.vue
@@ -0,0 +1,23 @@
+<template>
+  <ShadcnDropdownMenuSeparator v-if="showSeparator" />
+  <div v-for="(group, groupIndex) in category.items" :key="groupIndex">
+    <DropdownMenuItem :group="group" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import DropdownMenuItem from '@/components/dropdown-menu/DropdownMenuItem.vue';
+import type { DropdownMenuCategory as CategoryType } from '@/types/dropdown-menu.ts';
+import { DropdownMenuSeparator as ShadcnDropdownMenuSeparator } from '@/shadcn/ui/dropdown-menu';
+
+defineProps({
+  category: {
+    type: Object as () => CategoryType,
+    required: true,
+  },
+  showSeparator: {
+    type: Boolean,
+    required: true,
+  },
+});
+</script>
