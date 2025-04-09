dfb2bb3e749836eb4303949022c9d086942cfe59
LounisBou <lounis.bou@gmail.com>
2025-04-09 15:09:26 +0200

    Add initials possibility

--- demo/src/views/Avatar.vue
+++ demo/src/views/Avatar.vue
@@ -1,11 +1,15 @@
 <template>
-  <h1 class="mb-3">AVATAR :</h1>
-  <p class="mb-3">not square :</p>
-  <Avatar class="mb-3" :url="avatar_url" />
-  <p class="mb-3">square :</p>
-  <Avatar class="mb-3" :url="avatar_url" :is-square="true" :bg-color="'#ff0000'" />
-  <p class="mb-3">without avatar :</p>
-  <Avatar class="mb-3" :bg-color="'#ff00ff'" />
+  <div class="p-5">
+    <h1 class="mb-3">AVATAR :</h1>
+    <p class="mb-3">not square :</p>
+    <Avatar class="mb-3" :url="avatar_url" />
+    <p class="mb-3">square :</p>
+    <Avatar class="mb-3" :url="avatar_url" :is-square="true" :bg-color="'#ff0000'" />
+    <p class="mb-3">without avatar :</p>
+    <Avatar class="mb-3" :bg-color="'#ff00ff'" />
+    <p class="mb-3">without avatar with initials :</p>
+    <Avatar class="mb-3" :bg-color="'#000000'" :text-color="'#ff00ff'" :initials="'ab'" />
+  </div>
 </template>
 
 <script setup lang="ts">
--- src/components/avatar/Avatar.vue
+++ src/components/avatar/Avatar.vue
@@ -1,7 +1,12 @@
 <template>
   <ShadcnAvatar :class="props.isSquare ? 'h-8 w-8 rounded-lg' : ''" :style="{ backgroundColor: props.bgColor }">
     <ShadcnAvatarImage :src="props.url ?? ''" alt="Avatar" />
-    <ShadcnAvatarFallback :style="{ backgroundColor: props.bgColor }">?</ShadcnAvatarFallback>
+    <ShadcnAvatarFallback :style="{ backgroundColor: props.bgColor, color: props.textColor }">
+      <span v-if="props.initials" class="text-md font-bold">
+        {{ props.initials.toUpperCase() }}
+      </span>
+      <span v-else>?</span>
+    </ShadcnAvatarFallback>
   </ShadcnAvatar>
 </template>
 
@@ -14,13 +19,18 @@ import {
 } from '@/shadcn/ui/avatar';
 
 const props = defineProps({
-  isSquare: {
-    type: Boolean,
-    default: false,
-  },
   url: {
     type: String,
     required: false,
+  },
+  initials: {
+    type: String,
+    required: false,
+    validator: (value: string | null): boolean => {
+      if (value === null) return true;
+      // Check if the value is a string of 1 or 2 letters
+      return /^[a-zA-Z]{1,2}$/.test(value);
+    }
   },
   bgColor: {
     type: String as PropType<string | null>,
@@ -30,6 +40,19 @@ const props = defineProps({
       if (value === null) return true;
       return /^#([0-9A-F]{3}){1,2}$/i.test(value);
     },
+  },
+  textColor: {
+    type: String as PropType<string | null>,
+    required: false,
+    default: null,
+    validator: (value: string | null): boolean => {
+      if (value === null) return true;
+      return /^#([0-9A-F]{3}){1,2}$/i.test(value);
+    },
+  },
+  isSquare: {
+    type: Boolean,
+    default: false,
   },
 });
 </script>
