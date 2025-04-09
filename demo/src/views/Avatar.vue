7d5fd4a3fba5d2991e1ab59d2bcd9fd895ece2e4
Elias M <167002380+eliasm4r@users.noreply.github.com>
2025-04-09 15:12:19 +0200

    feat: avatar (#17)
    * add shadcn Avatar component + add Avatar component to view router
    
    * add background color for avatar
    
    * Add initials possibility
    
    ---------
    
    Co-authored-by: Tech Geonative <geo@MacBook-Air-de-Tech.local>
    Co-authored-by: LounisBou <lounis.bou@gmail.com>

---
+++ demo/src/views/Avatar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="p-5">
+    <h1 class="mb-3">AVATAR :</h1>
+    <p class="mb-3">not square :</p>
+    <Avatar class="mb-3" :url="avatar_url" />
+    <p class="mb-3">square :</p>
+    <Avatar class="mb-3" :url="avatar_url" :is-square="true" :bg-color="'#ff0000'" />
+    <p class="mb-3">without avatar :</p>
+    <Avatar class="mb-3" :bg-color="'#ff00ff'" />
+    <p class="mb-3">without avatar with initials :</p>
+    <Avatar class="mb-3" :bg-color="'#000000'" :text-color="'#ff00ff'" :initials="'ab'" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Avatar from '@/components/avatar/Avatar.vue';
+
+const avatar_url =
+  'https://avataaars.io/?avatarStyle=Transparent&topType=NoHair&accessoriesType=Blank&facialHairType=MoustacheFancy&facialHairColor=BrownDark&clotheType=ShirtScoopNeck&clotheColor=Blue03&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Light';
+</script>
+
+<style scoped></style>
---
+++ src/components/avatar/Avatar.vue
@@ -0,0 +1,58 @@
+<template>
+  <ShadcnAvatar :class="props.isSquare ? 'h-8 w-8 rounded-lg' : ''" :style="{ backgroundColor: props.bgColor }">
+    <ShadcnAvatarImage :src="props.url ?? ''" alt="Avatar" />
+    <ShadcnAvatarFallback :style="{ backgroundColor: props.bgColor, color: props.textColor }">
+      <span v-if="props.initials" class="text-md font-bold">
+        {{ props.initials.toUpperCase() }}
+      </span>
+      <span v-else>?</span>
+    </ShadcnAvatarFallback>
+  </ShadcnAvatar>
+</template>
+
+<script setup lang="ts">
+import { defineProps, type PropType } from 'vue';
+import {
+  Avatar as ShadcnAvatar,
+  AvatarImage as ShadcnAvatarImage,
+  AvatarFallback as ShadcnAvatarFallback,
+} from '@/shadcn/ui/avatar';
+
+const props = defineProps({
+  url: {
+    type: String,
+    required: false,
+  },
+  initials: {
+    type: String,
+    required: false,
+    validator: (value: string | null): boolean => {
+      if (value === null) return true;
+      // Check if the value is a string of 1 or 2 letters
+      return /^[a-zA-Z]{1,2}$/.test(value);
+    }
+  },
+  bgColor: {
+    type: String as PropType<string | null>,
+    required: false,
+    default: null,
+    validator: (value: string | null): boolean => {
+      if (value === null) return true;
+      return /^#([0-9A-F]{3}){1,2}$/i.test(value);
+    },
+  },
+  textColor: {
+    type: String as PropType<string | null>,
+    required: false,
+    default: null,
+    validator: (value: string | null): boolean => {
+      if (value === null) return true;
+      return /^#([0-9A-F]{3}){1,2}$/i.test(value);
+    },
+  },
+  isSquare: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
