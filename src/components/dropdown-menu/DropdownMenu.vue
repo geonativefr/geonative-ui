17b848dc849fcf6116610c52ec05d56b12459847
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-04-09 16:57:32 +0200

    add DropdownMenu components + add DropdownMenu to view

---
+++ src/components/dropdown-menu/DropdownMenu.vue
@@ -0,0 +1,42 @@
+<template>
+  <ShadcnDropdownMenu>
+    <ShadcnDropdownMenuTrigger>
+      <Button>{{ name }}</Button>
+    </ShadcnDropdownMenuTrigger>
+    <ShadcnDropdownMenuContent>
+      <ShadcnDropdownMenuLabel>{{ label }}</ShadcnDropdownMenuLabel>
+      <ShadcnDropdownMenuSeparator />
+      <div v-for="(category, categoryIndex) in items" :key="categoryIndex">
+        <DropdownMenuCategory :category="category" :show-separator="categoryIndex > 0" />
+      </div>
+    </ShadcnDropdownMenuContent>
+  </ShadcnDropdownMenu>
+</template>
+
+<script setup lang="ts">
+import {
+  DropdownMenu as ShadcnDropdownMenu,
+  DropdownMenuContent as ShadcnDropdownMenuContent,
+  DropdownMenuLabel as ShadcnDropdownMenuLabel,
+  DropdownMenuTrigger as ShadcnDropdownMenuTrigger,
+  DropdownMenuSeparator as ShadcnDropdownMenuSeparator,
+} from '@/shadcn/ui/dropdown-menu';
+import { Button } from '@/shadcn/ui/button';
+import DropdownMenuCategory from '@/components/dropdown-menu/DropdownMenuCategory.vue';
+import type { DropdownMenuCategory as CategoryType } from '@/types/dropdown-menu.ts';
+
+defineProps({
+  items: {
+    type: Array as () => CategoryType[],
+    required: true,
+  },
+  name: {
+    type: String,
+    required: false,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+});
+</script>
