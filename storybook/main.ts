4ef1537221743e4348c4297e47e80e053ebd13f7
LounisBou <lounis.bou@gmail.com>
2025-04-10 11:28:43 +0200

    Add storybook specific config files

---
+++ storybook/main.ts
@@ -0,0 +1,31 @@
+import type { StorybookConfig } from '@storybook/vue3-vite';
+import { resolve } from 'path';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+  ],
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  viteFinal: async (config) => {
+    // Add path aliases
+    if (config.resolve && config.resolve.alias) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': resolve(__dirname, '../src'),
+        '@storybook': resolve(__dirname, './src'),
+      };
+    }
+
+    return config;
+  },
+};
+
+export default config;
