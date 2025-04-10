49047e54af076479c8e4fe76eacd2b7da5c8e6e0
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-04-10 11:50:23 +0200

    change dropdown interface + add section in section

--- src/components/dropdown-menu/DropdownMenuItem.vue
+++ src/components/dropdown-menu/DropdownMenuItem.vue
@@ -1,19 +1,48 @@
 <template>
-  <ShadcnDropdownMenuItem v-for="item in props.group" :key="item.title" :disabled="item.disabled">
-    <a :href="item.link" class="flex items-center w-full">
-      {{ item.title }}
-      <span v-if="item.shortcut" class="ml-auto text-xs text-gray-500">{{ item.shortcut }}</span>
-    </a>
-  </ShadcnDropdownMenuItem>
+  <div>
+    <div v-if="props.item.sections && props.item.sections.length > 0">
+      <ShadcnDropdownMenuSub>
+        <ShadcnDropdownMenuSubTrigger>
+          <span>{{ props.item.label }}</span>
+        </ShadcnDropdownMenuSubTrigger>
+        <ShadcnDropdownMenuPortal>
+          <ShadcnDropdownMenuSubContent>
+            <div v-for="(subSection, index) in props.item.sections" :key="index">
+              <DropdownMenuSection :section="subSection" :show-separator="index > 0" />
+            </div>
+          </ShadcnDropdownMenuSubContent>
+        </ShadcnDropdownMenuPortal>
+      </ShadcnDropdownMenuSub>
+    </div>
+    <div v-else>
+      <ShadcnDropdownMenuItem :disabled="props.item.disabled">
+        <a v-if="props.item.url" :href="props.item.url" class="flex items-center w-full">
+          {{ props.item.label }}
+          <span v-if="props.item.shortcut" class="ml-auto text-xs text-gray-500">{{ props.item.shortcut }}</span>
+        </a>
+        <span v-else>
+          {{ props.item.label }}
+          <span v-if="props.item.shortcut" class="ml-auto text-xs text-gray-500">{{ props.item.shortcut }}</span>
+        </span>
+      </ShadcnDropdownMenuItem>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import type { DropdownMenuItem as ItemType } from '@/types/dropdown-menu.ts';
-import { DropdownMenuItem as ShadcnDropdownMenuItem } from '@/shadcn/ui/dropdown-menu';
+import {
+  DropdownMenuItem as ShadcnDropdownMenuItem,
+  DropdownMenuSub as ShadcnDropdownMenuSub,
+  DropdownMenuSubTrigger as ShadcnDropdownMenuSubTrigger,
+  DropdownMenuPortal as ShadcnDropdownMenuPortal,
+  DropdownMenuSubContent as ShadcnDropdownMenuSubContent,
+} from '@/shadcn/ui/dropdown-menu';
+import DropdownMenuSection from '@/components/dropdown-menu/DropdownMenuSection.vue';
 
 const props = defineProps({
-  group: {
-    type: Array as () => ItemType[],
+  item: {
+    type: Object as () => ItemType,
     required: true,
   },
 });
