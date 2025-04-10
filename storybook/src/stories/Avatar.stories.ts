607a07f1679d0c8f51c997df071f08117e4f00a4
LounisBou <lounis.bou@gmail.com>
2025-04-10 12:05:55 +0200

    Ignore storybook

--- storybook/src/stories/Avatar.stories.ts
+++ storybook/src/stories/Avatar.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
-import Avatar from '@/components/avatar/Avatar.vue';
+import Avatar from '../../../src/components/avatar/Avatar.vue';
 
 // More on how to set up stories at: https://storybook.js.org/docs/vue3/writing-stories/introduction
 const meta = {
