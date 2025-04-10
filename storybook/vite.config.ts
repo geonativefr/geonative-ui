f0f7f216823ee3173a97369b1ee7a915fe3f829e
LounisBou <lounis.bou@gmail.com>
2025-04-10 11:10:42 +0200

    Create Storybook Vite & TS config

--- storybook/vite.config.ts
+++ storybook/vite.config.ts
@@ -1,6 +1,11 @@
 import { StorybookConfig } from '@storybook/vue3-vite';
 import { resolve } from 'path';
+// Add this to avoid TypeScript errors when importing tailwindcss plugin
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import tailwindcss from '@tailwindcss/vite';
 
+// This is a Vite configuration file for Storybook.
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
@@ -22,6 +27,18 @@ const config: StorybookConfig = {
         '@': resolve(__dirname, '../', '../src'),
         '@storybook': resolve(__dirname, '../src'),
       };
+    }
+    // Add tailwindcss plugin
+    if (config.plugins) {
+      const tailwindPlugin = config.plugins.find(plugin => plugin.name === 'tailwindcss');
+      if (!tailwindPlugin) {
+        config.plugins.push(tailwindcss());
+      }
+    }
+    // Root directory
+    config.root = resolve(__dirname, '../storybook');
+    config.build = {
+      outDir: resolve(__dirname, '../storybook-dist'),
     }
     return config;
   },
