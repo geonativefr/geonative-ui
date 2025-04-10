a5b31e24305e7bd97204c232b417147b2ac987c6
LounisBou <lounis.bou@gmail.com>
2025-04-10 15:41:36 +0200

    Create Spinner and Progress loader

---
+++ demo/src/views/Loader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="m-5 flex flex-col gap-5">
+    <div class="flex flex-col gap-2">
+      <h2 class="text-2xl font-bold mb-5">Spinner</h2>
+      <div class="flex gap-5">
+        <Spinner size="sm" color="#ffff00" />
+        <Spinner size="md" />
+        <Spinner size="lg" color="#00ff00" />
+      </div>
+    </div>
+    <div class="flex flex-col gap-2 w-xl">
+      <h2 class="text-2xl font-bold mb-5">Progress</h2>
+      <div class="grid grid-cols-1 gap-5">
+          <Progress size="sm" :progress="progress" />
+          <Progress size="md" :progress="progress" />
+          <Progress size="lg" :progress="progress" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { Spinner, Progress } from '@/components/loader';
+
+const progress = ref(0  );
+function makeProgress(){
+  if (progress.value >= 100) {
+    progress.value = 0;
+  } else {
+    progress.value += 1;
+  }
+  setTimeout(() => {
+    makeProgress();
+  }, 100);
+}
+onMounted(() => {
+  makeProgress();
+});
+</script>
---
+++ src/components/loader/Progress.vue
@@ -0,0 +1,66 @@
+<template>
+  <Progress
+    :model-value="props.progress"
+    class="w-full border"
+    :class="`${sizeClass} bg-[${props.bgColor}]  border-[${props.borderColor}]`"
+  />
+</template>
+
+<script setup lang="ts">
+import { Progress } from '@/shadcn/ui/progress';
+import { computed } from 'vue';
+
+const props = defineProps({
+  size: {
+    type: String,
+    required: false,
+    default: 'md',
+    validator: (value: string) => {
+      return ['sm', 'md', 'lg'].includes(value)
+    }
+  },
+  progress: {
+    type: Number,
+    required: true,
+    validator: (value: number) => {
+      return (value >= 0 && value <= 100)
+    }
+  },
+  color: {
+    type: String,
+    required: false,
+    default: '#000000',
+    validator: (value: string) => {
+      return /^#[0-9A-F]{6}$/i.test(value)
+    }
+  },
+  bgColor: {
+    type: String,
+    required: false,
+    default: '#ffffff',
+    validator: (value: string) => {
+      return /^#[0-9A-F]{6}$/i.test(value)
+    }
+  },
+  borderColor: {
+    type: String,
+    required: false,
+    default: '#ffffff',
+    validator: (value: string) => {
+      return /^#[0-9A-F]{6}$/i.test(value)
+    }
+  }
+});
+const sizeClass = computed(() => {
+  switch (props.size) {
+    case 'sm':
+      return 'h-2';
+    case 'md':
+      return 'h-4';
+    case 'lg':
+      return 'h-6';
+    default:
+      return 'h-4';
+  }
+});
+</script>
