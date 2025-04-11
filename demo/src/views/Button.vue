2ba6a698d7b256ef42d8b2e00b651ad4f96b2fb1
LounisBou <lounis.bou@gmail.com>
2025-04-11 17:01:45 +0200

    Improve Button view demo

--- demo/src/views/Button.vue
+++ demo/src/views/Button.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="p-5">
-    <h1 class="mb-3">BUTTON :</h1>
+    <h1 class="mb-3">Button :</h1>
     <Button
       class="text-blue-500 bg-blue-100 hover:bg-blue-200 rounded-full px-4 py-2 shadow-md"
       :disabled="false"
-      @click="clickMethod"
+      @click="isClickedButton = !isClickedButton"
       size="lg"
     >
       Hello world !
     </Button>
-    <p v-if="clickedMessage" class="mt-3 text-green-500">{{ clickedMessage }}</p>
+    <p v-if="isClickedButton" class="mt-3 text-green-500">Button clicked !</p>
   </div>
 </template>
 
@@ -17,8 +17,6 @@
 import { ref } from 'vue';
 import Button from '@/components/button/Button.vue';
 
-const clickedMessage = ref('');
-const clickMethod = () => {
-  clickedMessage.value = clickedMessage.value ? '' : 'button clicked !';
-};
+const isClickedButton = ref(false);
+
 </script>
