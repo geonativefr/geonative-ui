1c6cf715005b0e94a3801da666bb12160447d16e
LounisBou <lounis.bou@gmail.com>
2025-04-11 17:30:56 +0200

    Remove all button keep only disabled one

--- demo/src/views/Button.vue
+++ demo/src/views/Button.vue
@@ -14,57 +14,11 @@
 
   <div class="p-5 space-y-4">
     <h1 class="mb-3">Exemples de styles de boutons :</h1>
-
-    <!-- Bouton primaire -->
-    <Button
-      class="bg-blue-500 text-white hover:bg-blue-600 rounded-lg px-4 py-2 shadow-md"
-      :disabled="false"
-      size="md"
-    >
-      Bouton primaire
-    </Button>
-
-    <!-- Bouton secondaire -->
-    <Button
-      class="bg-gray-500 text-white hover:bg-gray-600 rounded-lg px-4 py-2 shadow-md"
-      :disabled="false"
-      size="md"
-    >
-      Bouton secondaire
-    </Button>
-
-    <!-- Bouton succès -->
-    <Button
-      class="bg-green-500 text-white hover:bg-green-600 rounded-lg px-4 py-2 shadow-md"
-      :disabled="false"
-      size="md"
-    >
-      Bouton succès
-    </Button>
-
-    <!-- Bouton danger -->
-    <Button
-      class="bg-red-500 text-white hover:bg-red-600 rounded-lg px-4 py-2 shadow-md"
-      :disabled="false"
-      size="md"
-    >
-      Bouton danger
-    </Button>
-
-    <!-- Bouton avec bordure -->
-    <Button
-      class="border border-blue-500 text-blue-500 hover:bg-blue-100 rounded-lg px-4 py-2 shadow-md"
-      :disabled="false"
-      size="md"
-    >
-      Bouton avec bordure
-    </Button>
-
     <!-- Bouton désactivé -->
     <Button
-      class="bg-gray-300 text-gray-500 rounded-lg px-4 py-2 shadow-md"
       :disabled="true"
       size="md"
+      @click="isClickedButton = !isClickedButton"
     >
       Bouton désactivé
     </Button>
