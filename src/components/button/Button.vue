e27c11f52abb4182826ea2e22ab4c510f5003fa7
LounisBou <lounis.bou@gmail.com>
2025-04-18 14:21:19 +0200

    Make button inline

--- src/components/button/Button.vue
+++ src/components/button/Button.vue
@@ -1,24 +1,26 @@
 <template>
-  <button
-    :class="
-      twMerge(
-        `relative flex items-center rounded-md bg-primary text-white shadow-md m-2 px-4 py-2 cursor-pointer ${sizeClass}`,
-        props.disabled || props.loading ? 'cursor-not-allowed' : 'hover:opacity-80',
-        props.disabled ? 'opacity-80' : '',
-        props.class
-      )
-    "
-    :disabled="props.disabled || props.loading"
-  >
-    <div class="flex items-center gap-2 whitespace-nowrap" :class="{ invisible: props.loading }">
-      <slot>{{ props.label }}</slot>
+  <div class="inline-block">
+    <button
+      :class="
+        twMerge(
+          `relative flex items-center rounded-md bg-primary text-white shadow-md px-4 py-2 cursor-pointer ${sizeClass}`,
+          props.disabled || props.loading ? 'cursor-not-allowed' : 'hover:opacity-80',
+          props.disabled ? 'opacity-80' : '',
+          props.class
+        )
+      "
+      :disabled="props.disabled || props.loading"
+    >
+      <div class="flex items-center gap-2 whitespace-nowrap" :class="{ invisible: props.loading }">
+        <slot>{{ props.label }}</slot>
 
-    </div>
-    <div v-if="props.loading" class="absolute inset-0 flex items-center justify-center gap-2">
-      <Spinner :size="props.size" />
-      <span v-if="props.loadingText">{{ props.loadingText }}</span>
-    </div>
-  </button>
+      </div>
+      <div v-if="props.loading" class="absolute inset-0 flex items-center justify-center gap-2">
+        <Spinner :size="props.size" />
+        <span v-if="props.loadingText">{{ props.loadingText }}</span>
+      </div>
+    </button>
+  </div>
 </template>
 
 <script setup lang="ts">
