9982820849e1613853ab9b87c5665af3aba444ed
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-04-24 17:26:40 +0200

    add v-else-if startsWith('http')

--- src/components/dropdown-menu/DropdownMenuItem.vue
+++ src/components/dropdown-menu/DropdownMenuItem.vue
@@ -1,22 +1,8 @@
 <template>
   <div v-if="props.item.label">
       <ShadcnDropdownMenuItem :disabled="props.item.action == '' || props.item.disabled">
-        <router-link v-if="typeof props.item.action === 'string'" :to="props.item.action" class="flex justify-between items-center w-full">
-          <div class="flex items-center gap-2">
-            <Icon
-              v-if="item.iconProps"
-              :name="item.iconProps.name"
-              :class="item.iconProps.class"
-              :source="item.iconProps.source"
-              :type="item.iconProps.type"
-            />
-            <span>{{ props.item.label }}</span>
-          </div>
-          <ShadcnDropdownMenuShortcut v-if="props.item.shortcut" class="ml-10">
-            {{ props.item.shortcut }}
-          </ShadcnDropdownMenuShortcut>
-        </router-link>
-        <div v-else-if="typeof props.item.action === 'function'" @click="props.item.action" class="flex justify-between items-center w-full">
+
+        <div v-if="typeof props.item.action === 'function'" @click="props.item.action" class="flex justify-between items-center w-full">
           <div class="flex items-center gap-2">
             <Icon
               v-if="item.iconProps"
@@ -31,6 +17,39 @@
             {{ props.item.shortcut }}
           </ShadcnDropdownMenuShortcut>
         </div>
+
+        <a v-else-if="typeof props.item.action === 'string' && props.item.action.startsWith('http')" :href="props.item.action" class="flex justify-between items-center w-full">
+          <div class="flex items-center gap-2">
+            <Icon
+              v-if="item.iconProps"
+              :name="item.iconProps.name"
+              :class="item.iconProps.class"
+              :source="item.iconProps.source"
+              :type="item.iconProps.type"
+            />
+            <span>{{ props.item.label }}</span>
+          </div>
+          <ShadcnDropdownMenuShortcut v-if="props.item.shortcut" class="ml-10">
+            {{ props.item.shortcut }}
+          </ShadcnDropdownMenuShortcut>
+        </a>
+
+        <router-link v-else-if="typeof props.item.action === 'string'" :to="props.item.action" class="flex justify-between items-center w-full">
+          <div class="flex items-center gap-2">
+            <Icon
+              v-if="item.iconProps"
+              :name="item.iconProps.name"
+              :class="item.iconProps.class"
+              :source="item.iconProps.source"
+              :type="item.iconProps.type"
+            />
+            <span>{{ props.item.label }}</span>
+          </div>
+          <ShadcnDropdownMenuShortcut v-if="props.item.shortcut" class="ml-10">
+            {{ props.item.shortcut }}
+          </ShadcnDropdownMenuShortcut>
+        </router-link>
+
         <div v-else class="flex justify-between items-center w-full">
           <div class="flex items-center gap-2">
             <Icon
