ea4d134c94961486eef93f383cd78605968ae179
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-04-24 09:30:59 +0200

    revert change in DropdownMenu

--- demo/src/views/DropdownMenu.vue
+++ demo/src/views/DropdownMenu.vue
@@ -4,11 +4,11 @@
     <div class="place-items-center">
       <p class="mb-3">default:</p>
       <DropdownMenu :sections="dropdownMenu.sections">
-          <Button class="text-blue-500 bg-blue-100 rounded-full">Menu 1</Button>
+        <Button class="text-blue-500 bg-blue-100 rounded-full">Menu 1</Button>
       </DropdownMenu>
       <p class="mb-3 mt-30">position right + align end :</p>
       <DropdownMenu :sections="dropdownMenu.sections" :label="dropdownMenu.label" position="right" align="end">
-          <Button class="text-pink-500 bg-pink-100 rounded-full">Menu 2</Button>
+        <Button class="text-pink-500 bg-pink-100 rounded-full">Menu 2</Button>
       </DropdownMenu>
     </div>
   </div>
