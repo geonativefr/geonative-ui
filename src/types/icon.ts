6e4ceb7ae7836ce61e78fd8cd9c23307d1a6ea63
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-04-24 14:28:12 +0200

    move IconProps

--- src/types/icon.ts
+++ src/types/icon.ts
@@ -1,2 +1,9 @@
 export type IconSource = 'svg' | 'lucide' | 'heroicons';
 export type IconType = 'solid' | 'outline' | undefined;
+
+export interface IconProps {
+  name: string;
+  source: IconSource;
+  type?: IconType;
+  class?: string;
+}
