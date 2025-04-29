d3153118d252211ed7e36b1e95d6bada48ff67f7
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-04-29 14:58:44 +0200

    create account switcher + fix name
    # Conflicts:
    #	demo/src/routes.ts
    #	src/components/index.ts
    
    # Conflicts:
    #	src/components/index.ts
    
    # Conflicts:
    #	demo/src/routes.ts
    
    # Conflicts:
    #	src/components/index.ts

---
+++ src/components/account-switcher/AccountSwitcher.vue
@@ -0,0 +1,25 @@
+<template>
+  <DropdownMenu :dropdown="props.dropdown" position="right" align="start">
+    <Button size="lg" class="bg-white hover:bg-gray-100 text-black rounded-lg px-2 py-6">
+      <div class="flex items-center gap-2">
+        <slot />
+        <ChevronsUpDown class="size-5 ml-6" />
+      </div>
+    </Button>
+    <template #header>
+      <div class="flex items-center gap-2">
+        Change Account
+      </div>
+    </template>
+  </DropdownMenu>
+</template>
+
+<script setup lang="ts">
+import { ChevronsUpDown } from 'lucide-vue-next';
+import { Button, DropdownMenu } from '@geonative/ui/components';
+import type { DropdownMenuType } from '@geonative/ui/types';
+
+const props = defineProps<{
+  dropdown: DropdownMenuType;
+}>();
+</script>
