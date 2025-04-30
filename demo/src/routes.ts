656b5621995b629274fdba7773591ac34e480f40
LounisBou <lounis.bou@gmail.com>
2025-04-30 11:39:45 +0200

    Add shadcn form and auto-form

--- demo/src/routes.ts
+++ demo/src/routes.ts
@@ -1,12 +1,22 @@
 import type { RouteRecord } from '@geonative/ui/types';
 import { createRoute } from '@geonative/ui/helpers';
 
+/**
+ * This file contains the route definitions for the demo application.
+ * (Please keep it in alphabetical order.)
+ */
 const routes: Array<RouteRecord> = [
   createRoute({
     path: '/',
     name: 'home',
     component: () => import('@/views/Home.vue'),
     menuLabel: 'Home',
+  }),
+  createRoute({
+    path: '/action',
+    name: 'action',
+    component: () => import('@/views/Action.vue'),
+    menuLabel: 'Action',
   }),
   createRoute({
     path: '/avatar',
@@ -21,10 +31,22 @@ const routes: Array<RouteRecord> = [
     menuLabel: 'Breadcrumb',
   }),
   createRoute({
-    path: '/loader',
-    name: 'loader',
-    component: () => import('@/views/Loader.vue'),
-    menuLabel: 'Loader',
+    path: '/button',
+    name: 'button',
+    component: () => import('@/views/Button.vue'),
+    menuLabel: 'Button',
+  }),
+  createRoute({
+    path: '/dropdown-menu',
+    name: 'dropdown-menu',
+    component: () => import('@/views/DropdownMenu.vue'),
+    menuLabel: 'Dropdown Menu',
+  }),
+  createRoute({
+    path: '/form',
+    name: 'form',
+    component: () => import('@/views/Form.vue'),
+    menuLabel: 'Form Builder',
   }),
   createRoute({
     path: '/icon',
@@ -33,22 +55,10 @@ const routes: Array<RouteRecord> = [
     menuLabel: 'Icon',
   }),
   createRoute({
-    path: '/button',
-    name: 'button',
-    component: () => import('@/views/Button.vue'),
-    menuLabel: 'Button',
-  }),
-  createRoute({
-    path: '/action',
-    name: 'action',
-    component: () => import('@/views/Action.vue'),
-    menuLabel: 'Action',
-  }),
-  createRoute({
-    path: '/dropdown-menu',
-    name: 'dropdown-menu',
-    component: () => import('@/views/DropdownMenu.vue'),
-    menuLabel: 'Dropdown Menu',
+    path: '/loader',
+    name: 'loader',
+    component: () => import('@/views/Loader.vue'),
+    menuLabel: 'Loader',
   }),
   createRoute({
     path: '/navbar-dropdown',
---
+++ src/components/form/FormBuilder.vue
@@ -0,0 +1,8 @@
+<template>
+  <AutoForm :form="form" :schema="schema" />
+</template>
+
+<script setup lang="ts">
+import { AutoForm } from '@geonative/ui/shadcn/ui/
+
+</script>
