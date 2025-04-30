2db34fdce45637e35eca9dd897151c1e6b9dcede
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-04-30 16:59:41 +0200

    remove props.item.icon Props in DropdownMenuSection

--- src/components/dropdown-menu/DropdownMenuSection.vue
+++ src/components/dropdown-menu/DropdownMenuSection.vue
@@ -5,7 +5,7 @@
       <ShadcnDropdownMenuSub>
         <ShadcnDropdownMenuSubTrigger>
           <div class="flex items-center gap-2">
-            <Icon v-if="props.item.iconProps" v-bind="props.item.iconProps" />
+            <Icon v-if="item.iconProps" v-bind="item.iconProps" />
             <span>{{ item.label }}</span>
           </div>
         </ShadcnDropdownMenuSubTrigger>
