cc00fa3534c8a73ff3ebfc2f16de8f9f60c49fa3
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-05-02 09:22:47 +0200

    change Account to Items

--- demo/src/views/ItemsSwitcher.vue
+++ demo/src/views/ItemsSwitcher.vue
@@ -11,26 +11,26 @@ import { ItemsSwitcher } from '@geonative/ui/components';
 const items = [
   {
     id: 1,
-    name: 'alice',
-    avatar: 'https://avataaars.io/?avatarStyle=Transparent&topType=LongHairStraight&accessoriesType=Kurt&facialHairType=Blank&clotheType=ShirtScoopNeck&clotheColor=White&eyeType=Happy&eyebrowType=RaisedExcited&mouthType=Smile&skinColor=Light',
+    label: 'alice',
+    icon: 'https://avataaars.io/?avatarStyle=Transparent&topType=LongHairStraight&accessoriesType=Kurt&facialHairType=Blank&clotheType=ShirtScoopNeck&clotheColor=White&eyeType=Happy&eyebrowType=RaisedExcited&mouthType=Smile&skinColor=Light',
     color: 'bg-pink-500',
   },
   {
     id: '2',
-    name: 'braum',
-    avatar:
+    label: 'braum',
+    icon:
       'https://avataaars.io/?avatarStyle=Transparent&topType=NoHair&accessoriesType=Blank&facialHairType=MoustacheFancy&facialHairColor=BrownDark&clotheType=ShirtScoopNeck&clotheColor=Blue03&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Light',
     color: 'bg-blue-500',
   },
   {
     id: 3,
-    name: 'cloud',
-    avatar:
+    label: 'cloud',
+    icon:
       'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairDreads02&accessoriesType=Blank&hairColor=BlondeGolden&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light',
   },
   {
     id: '4',
-    name: 'dante',
+    label: 'dante',
     color: 'bg-yellow-500',
   },
 ];
