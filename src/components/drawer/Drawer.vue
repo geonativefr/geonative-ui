1569a91f3bdd9801efb16bbfda7bfd5edee2c24a
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-05-15 09:49:34 +0200

    add drower component and add an example in view

--- src/components/drawer/Drawer.vue
+++ src/components/drawer/Drawer.vue
@@ -1,11 +1,52 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
+  <ShadcnDrawer :direction="props.direction" :should-scale-background="props.scale" :modal="props.modal" >
+    <ShadcnDrawerTrigger as-child>
+      <slot name="trigger" />
+    </ShadcnDrawerTrigger>
 
+    <ShadcnDrawerContent >
+      <div class="mx-auto w-full max-w-sm">
+        <ShadcnDrawerHeader>
+          <slot name="header" />
+        </ShadcnDrawerHeader>
+
+        <div class="p-4 pb-0">
+          <div class="flex items-center justify-center space-x-2">
+            <slot />
+          </div>
+        </div>
+
+        <ShadcnDrawerFooter>
+          <slot name="footer" />
+          <DrawerClose>
+            <Button class="w-auto">
+              Cancel
+            </Button>
+          </DrawerClose>
+        </ShadcnDrawerFooter>
+      </div>
+    </ShadcnDrawerContent>
+  </ShadcnDrawer>
 </template>
 
-<style scoped>
+<script setup lang="ts">
+import {
+  Drawer as ShadcnDrawer,
+  DrawerTrigger as ShadcnDrawerTrigger,
+  DrawerFooter as ShadcnDrawerFooter,
+  DrawerHeader as ShadcnDrawerHeader,
+  DrawerContent as ShadcnDrawerContent, DrawerClose,
+} from '@geonative/ui/shadcn/ui/drawer';
+import { Button } from '@geonative/ui/components';
 
-</style>
+const props = withDefaults(defineProps<{
+  direction?: 'top' | 'bottom' | 'left' | 'right' | undefined;
+  scale?: boolean
+  modal?: boolean
+}>(), {
+  direction: 'right',
+  scale: true,
+  modal: true,
+});
+
+</script>
