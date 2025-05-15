1b2c13d6588ea37f2d3a6f3c54005c71fe7ef9a5
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-05-15 09:49:34 +0200

    add drower component and add an example in view

--- demo/src/views/Drawer.vue
+++ demo/src/views/Drawer.vue
@@ -1,7 +1,31 @@
 <template>
+<div class="flex items-center justify-center min-h-screen p-5">
+  <Drawer>
+      <template #trigger>
+        <Button class="bg-green-500 w-auto" size="lg">Button LG</Button>
+      </template>
 
+      <template #header>
+        <DrawerTitle>
+          This is a Title
+        </DrawerTitle>
+        <DrawerDescription>
+          This is a Description
+        </DrawerDescription>
+      </template>
+
+      <span>This is a Content</span>
+
+      <template #footer>
+
+      </template>
+
+    </Drawer>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { Drawer, Button } from '@geonative/ui/components'
+import { DrawerDescription, DrawerTitle } from '@geonative/ui/shadcn/ui/drawer';
 
 </script>
