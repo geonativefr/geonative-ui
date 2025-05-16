51d6bda850a949371049e559598df51159182dfe
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-05-16 10:59:54 +0200

    removed all shadcn code from demo + create SidebarGroup

---
+++ src/components/sidebar/SidebarGroup.vue
@@ -0,0 +1,10 @@
+<template>
+  <ShadcnSidebarGroup>
+    <slot />
+  </ShadcnSidebarGroup>
+</template>
+
+<script setup lang="ts">
+import { SidebarGroup as ShadcnSidebarGroup } from '@geonative/ui/shadcn/ui/sidebar';
+</script>
+
