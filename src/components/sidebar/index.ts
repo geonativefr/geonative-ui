3284cafbc68be663d01494b7fe05de59842b9da6
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-05-16 10:59:54 +0200

    removed all shadcn code from demo + create SidebarGroup

--- src/components/sidebar/index.ts
+++ src/components/sidebar/index.ts
@@ -3,4 +3,5 @@ export { default as SidebarInset } from './SidebarInset.vue';
 export { default as SidebarProvider } from './SidebarProvider.vue';
 export { default as SidebarTrigger } from './SidebarTrigger.vue';
 export { default as SidebarNav } from './SidebarNav.vue';
+export { default as SidebarGroup } from './SidebarGroup.vue';
 
