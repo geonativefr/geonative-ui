dc540c78972c898af8d355efe5527199d28592c4
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-05-16 10:16:55 +0200

    remove shadcn component in AppLayout + fix header scrolling with the view

---
+++ src/components/sidebar/SidebarTrigger.vue
@@ -0,0 +1,17 @@
+<template>
+  <ShadcnSidebarTrigger />
+  <ShadcnSeparator :orientation="props.orientation" />
+</template>
+
+<script setup lang="ts">
+import { SidebarTrigger as ShadcnSidebarTrigger } from '@geonative/ui/shadcn/ui/sidebar';
+import { Separator as ShadcnSeparator } from '@geonative/ui/shadcn/ui/separator';
+
+const props = withDefaults(
+  defineProps<{
+    orientation?: 'horizontal' | 'vertical';
+  }>(),
+  { orientation: 'vertical' }
+);
+</script>
+
