e7f2c720d5cdfc13b93b22c99598d2aba083250b
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-05-21 14:48:20 +0200

    fix theme + icon color

--- src/components/dropdown-menu/DropdownMenuItem.vue
+++ src/components/dropdown-menu/DropdownMenuItem.vue
@@ -3,9 +3,17 @@
     <Action :disabled="isDisabled" :action-click="props.item.clickAction || ''" class="cursor-not-allowed">
       <ShadcnDropdownMenuItem
         :disabled="isDisabled"
+        class="text-foreground"
       >
         <div class="flex items-center gap-2">
-          <Icon v-if="props.item.iconProps" v-bind="props.item.iconProps" />
+          <Icon
+            v-if="props.item.iconProps"
+            :name="props.item.iconProps.name"
+            :source="props.item.iconProps.source"
+            :type="props.item.iconProps.type"
+            :avatar-props="props.item.iconProps.avatarProps"
+            :class="twMerge('text-foreground', props.item.iconProps.class)"
+          />
           <span>{{ props.item.label }}</span>
         </div>
         <ShadcnDropdownMenuShortcut v-if="props.item.shortcut">
@@ -23,6 +31,7 @@ import {
   DropdownMenuShortcut as ShadcnDropdownMenuShortcut,
 } from '@geonative/ui/shadcn/ui/dropdown-menu';
 import type { DropdownMenuItemType } from '@geonative/ui/types';
+import { twMerge } from 'tailwind-merge';
 
 const props = defineProps<{
   item: DropdownMenuItemType;
---
+++ src/components/dropdown-menu/DropdownMenuSub.vue
@@ -0,0 +1,40 @@
+<template>
+  <ShadcnDropdownMenuSub>
+    <ShadcnDropdownMenuSubTrigger>
+      <div class="flex items-center gap-2 w-full">
+        <Icon
+          v-if="props.item.iconProps"
+          :name="props.item.iconProps.name"
+          :source="props.item.iconProps.source"
+          :type="props.item.iconProps.type"
+          :avatar-props="props.item.iconProps.avatarProps"
+          :class="twMerge('text-foreground', props.item.iconProps.class)"
+        />
+        <span>{{ props.item.label }}</span>
+      </div>
+    </ShadcnDropdownMenuSubTrigger>
+    <ShadcnDropdownMenuPortal>
+      <ShadcnDropdownMenuSubContent class="bg-background text-foreground">
+        <div v-for="(section, subIndex) in props.item.sections" :key="subIndex">
+          <DropdownMenuSection :section="section" :show-separator="subIndex !== 0" />
+        </div>
+      </ShadcnDropdownMenuSubContent>
+    </ShadcnDropdownMenuPortal>
+  </ShadcnDropdownMenuSub>
+</template>
+
+<script setup lang="ts">
+import { DropdownMenuSection, Icon } from '@geonative/ui/components';
+import {
+  DropdownMenuSub as ShadcnDropdownMenuSub,
+  DropdownMenuSubTrigger as ShadcnDropdownMenuSubTrigger,
+  DropdownMenuPortal as ShadcnDropdownMenuPortal,
+  DropdownMenuSubContent as ShadcnDropdownMenuSubContent,
+} from '@geonative/ui/shadcn/ui/dropdown-menu';
+import type { DropdownMenuItemType } from '@geonative/ui/types';
+import { twMerge } from 'tailwind-merge';
+
+const props = defineProps<{
+  item: DropdownMenuItemType;
+}>();
+</script>
