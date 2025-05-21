9ea5e422071c44789655c3aa92e23fb9697e40f2
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-05-21 09:48:53 +0200

    fix Action in DropdownMenuItem

--- src/components/dropdown-menu/DropdownMenuItem.vue
+++ src/components/dropdown-menu/DropdownMenuItem.vue
@@ -1,16 +1,16 @@
 <template>
   <div v-if="props.item.label">
-    <ShadcnDropdownMenuItem :disabled="!props.item.clickAction || props.item.clickAction == '' || props.item.disabled">
-      <Action :actionClick="props.item.clickAction || ''" class="flex justify-between items-center w-full">
-        <div class="flex items-center gap-2">
-          <Icon v-if="props.item.iconProps" v-bind="props.item.iconProps" />
-          <span>{{ props.item.label }}</span>
-        </div>
-        <ShadcnDropdownMenuShortcut v-if="props.item.shortcut" class="ml-10">
-          {{ props.item.shortcut }}
-        </ShadcnDropdownMenuShortcut>
-      </Action>
-    </ShadcnDropdownMenuItem>
+    <Action :actionClick="props.item.clickAction || ''" >
+      <ShadcnDropdownMenuItem :disabled="!props.item.clickAction || props.item.clickAction == '' || props.item.disabled" class="flex justify-between items-center w-full">
+          <div class="flex items-center gap-2">
+            <Icon v-if="props.item.iconProps" v-bind="props.item.iconProps" />
+            <span>{{ props.item.label }}</span>
+          </div>
+          <ShadcnDropdownMenuShortcut v-if="props.item.shortcut" class="ml-10">
+            {{ props.item.shortcut }}
+          </ShadcnDropdownMenuShortcut>
+      </ShadcnDropdownMenuItem>
+    </Action>
   </div>
 </template>
 
