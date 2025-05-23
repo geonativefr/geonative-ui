75e3b58bb64d861facbfff9cfb155e958e4a1554
Elias M <167002380+eliasm4r@users.noreply.github.com>
2025-05-23 17:37:03 +0200

    feat: account-switcher (#49)

---
+++ demo/src/views/ItemsSwitcher.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="p-5">
+    <h1 class="mb-3">Dropdown Switcher:</h1>
+    <ItemsSwitcher :dropdown="dropdown" :active-item="activeItem">
+      <span class="flex items-center gap-2 font-bold">Change Account</span>
+    </ItemsSwitcher>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ItemsSwitcher } from '@geonative/ui/components';
+import type { DropdownMenuType } from '@geonative/ui/types';
+import { ref } from 'vue';
+
+interface Item {
+  id: string | number;
+  label: string;
+  icon?: string;
+  color?: string;
+}
+
+const items = [
+  {
+    id: 1,
+    label: 'alice',
+    icon: 'https://avataaars.io/?avatarStyle=Transparent&topType=LongHairStraight&accessoriesType=Kurt&facialHairType=Blank&clotheType=ShirtScoopNeck&clotheColor=White&eyeType=Happy&eyebrowType=RaisedExcited&mouthType=Smile&skinColor=Light',
+    color: 'bg-pink-500',
+  },
+  {
+    id: '2',
+    label: 'braum',
+    icon: 'https://avataaars.io/?avatarStyle=Transparent&topType=NoHair&accessoriesType=Blank&facialHairType=MoustacheFancy&facialHairColor=BrownDark&clotheType=ShirtScoopNeck&clotheColor=Blue03&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Light',
+    color: 'bg-blue-500',
+  },
+  {
+    id: 3,
+    label: 'cloud',
+    icon: 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairDreads02&accessoriesType=Blank&hairColor=BlondeGolden&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light',
+  },
+  {
+    id: '4',
+    label: 'dante',
+    color: 'bg-yellow-500',
+  },
+];
+
+const activeItem = ref<Item | undefined>(items[0]);
+
+function changeItem(itemId: Item['id']) {
+  const foundItem = items.find((item) => item.id === itemId);
+  if (foundItem) {
+    activeItem.value = foundItem;
+  }
+}
+
+const dropdown: DropdownMenuType = {
+  sections: [
+    items.map((item) => ({
+      label: item.label,
+      class: 'flex items-center gap-2',
+      clickAction: () => {
+        changeItem(item.id);
+        accountHasChange(item);
+      },
+      iconProps: {
+        name: item.label,
+        source: 'avatar',
+        class: 'size-8',
+        avatarProps: { url: item.icon, initials: item.label.slice(0, 2), class: item.color },
+      },
+    })),
+    [
+      {
+        label: 'Add Account',
+        clickAction: () => alert('Add Account features coming soon'),
+        iconProps: { name: 'plus-circle', source: 'heroicons', type: 'outline', class: 'h-6 w-6' },
+      },
+    ],
+  ],
+};
+
+function accountHasChange(item: Item) {
+  console.log(`User ${item.label} has been selected`);
+}
+</script>
---
+++ src/components/items-switcher/ItemsSwitcher.vue
@@ -0,0 +1,31 @@
+<template>
+  <DropdownMenu :dropdown="props.dropdown" position="right" align="start">
+    <Button size="lg" class="bg-background text-foreground rounded-lg px-2 py-6">
+      <div class="flex items-center justify-between gap-2">
+        <Avatar
+          :url="props.activeItem?.icon"
+          :class="props.activeItem?.color"
+          :initials="props.activeItem?.label.slice(0, 2)"
+        />
+        <span class="flex-1 text-center text-sm">
+          <span class="font-semibold">{{ props.activeItem?.label }}</span>
+        </span>
+        <ChevronsUpDown class="size-5 ml-6" />
+      </div>
+    </Button>
+    <template #header>
+      <slot />
+    </template>
+  </DropdownMenu>
+</template>
+
+<script setup lang="ts">
+import { ChevronsUpDown } from 'lucide-vue-next';
+import { Avatar, Button, DropdownMenu } from '@geonative/ui/components';
+import type { DropdownMenuType, Item } from '@geonative/ui/types';
+
+const props = defineProps<{
+  dropdown: DropdownMenuType;
+  activeItem?: Item;
+}>();
+</script>
