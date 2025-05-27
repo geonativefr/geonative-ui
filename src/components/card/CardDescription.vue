dcaf7a58e6997937ef3b16ca4e64a38eb4cb1b24
Tech Geonative <geo@MacBook-Air-de-Tech.local>
2025-05-27 14:20:16 +0200

    add card in demo + fix shadcn import
    # Conflicts:
    #	demo/src/routes.ts
    #	src/components/index.ts

---
+++ src/components/card/CardDescription.vue
@@ -0,0 +1,9 @@
+<template>
+  <ShadcnCardDescription>
+    <slot />
+  </ShadcnCardDescription>
+</template>
+
+<script setup lang="ts">
+import { CardDescription as ShadcnCardDescription } from '@geonative/ui/shadcn/ui/card';
+</script>
---
+++ src/components/card/CardTitle.vue
@@ -0,0 +1,9 @@
+<template>
+  <ShadcnCardTitle>
+    <slot />
+  </ShadcnCardTitle>
+</template>
+
+<script setup lang="ts">
+import { CardTitle as ShadcnCardTitle } from '@geonative/ui/shadcn/ui/card';
+</script>
